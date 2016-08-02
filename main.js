3bf689949e09b73044dee97d06445e030de60efa
KrysD <dumora.christophe@gmail.com>
2016-08-02 17:32:42 +0200

    use crossfilter to prepare multi graph filtering

--- main.js
+++ main.js
@@ -6,158 +6,113 @@ var d3brush = require('d3-brush');
 var crossfilter = require('crossfilter');
 var parse = require('csv-parse'); //read line by line csv file
 
+json.forEach(function(d, i) {
+    d.index = i;
+    d.PetalLength = +d.PetalLength;
+    d.PetalWidth = +d.PetalWidth;
+    d.SepalLength = +d.SepalLength;
+    d.SepalWidth = +d.SepalWidth;
+});
+
 // Create the crossfilter for the relevant dimensions and groups.
 var iris = crossfilter(json),
-  all = iris.groupAll(), //dimension with all the data
-  speciesDim = iris.dimension(function(d) { return d.Species; }); //dimension depending on the species
+	all = iris.groupAll(),
+	Species = iris.dimension(function(d) { return d.Species; }),
+	gSpecies = Species.group().reduceCount().all(), //Group to cardinality of each Species
+    PetalLength = iris.dimension(function(d) {return d.PetalLength;}),
+    gPetalLength = PetalLength.group(function(d) {return Math.floor(d);}); //Create one group each centimeter
 
-//Calculate K for histograms
-
-var K = Math.ceil(1+Math.log2(all.reduceCount().value()));
-console.log(K);
-
-
-//Test some crossfilter features 
-//Not Run
-var speciesDimension = iris.dimension(function(d) { return d.Species; });
-speciesDimension.filter("setosa")
-
-var n = iris.groupAll().reduceCount().value();
-console.log("There are " + n + " setosa in my house.")
-
-speciesDimension.filterAll()
-var n = iris.groupAll().reduceCount().value();
-console.log("There are " + n + " setosa in my house.")
-
-var countSpecies = speciesDimension.group().reduceCount();
-var a = countSpecies.top(4);
-
-var test = iris.dimension(function(d) { return Math.min(1999, d.SepalLength); })
-var distances = test.group(function(d) { return Math.floor(d / 50) * 50; });
-//End not run
-
-
-var SepalLength = [];
-var Species = [];
-
-json.forEach(function(element){
-SepalLength.push(parseFloat(element.SepalLength));
-Species.push(element.Species);
-})
-
-var counts = {};
-for (var i = 0; i < Species.length; i++) {
-    counts[Species[i]] = 1 + (counts[Species[i]] || 0);
-}
-
-/*console.log(Object.keys(counts));*/
-//Width and height
-var w = 800;
-var h = 250;
-var barPadding = 5;
-var w2 = w + SepalLength.length*barPadding
-var padding = 30;
-var margin = {
-'top'    : 5, 
-'right'  : 20, 
-'bottom' : 20, 
-'left'   : 50 
+/*Species.filter("setosa") //include only setosa
+var n = iris.groupAll().reduceCount().value(); //count number of element
+console.log("1: "+n) //50
+Species.filterFunction(function(d) { return d =='setosa' || d=='versicolor'; });//include only setosa anv versicolor
+var n = iris.groupAll().reduceCount().value(); //count number of element
+console.log("2: "+n)//150 
+Species.filterAll() //reset filter 
+var n = iris.groupAll().reduceCount().value(); //count number of element
+console.log("3: "+n)//150 
+var n = console.log(gPetalLength.top(Infinity));
+console.log("4: "+n)//count number of plants in each petalLength group [1=50,,3=11,4=43,5=35,6=11]*/
+/*PetalLength.filter([5, 6])*/
+var data = gSpecies;
+console.log(gSpecies);
+var margin = {top: 10,
+    right: 31,
+    bottom: 32,
+    left: 10
 };
 
-var xScale = d3.scale.ordinal()
-                .domain(d3.range(SepalLength.length))
-                .rangeRoundBands([padding, w - padding * 2]);
+var width = 500 - margin.left - margin.right,
+    height = 100 - margin.top - margin.bottom;
 
-var yScale = d3.scale.linear()
-			.domain([0, d3.max(SepalLength)])
-			.range([h - margin.top,0 + margin.top,]);
+var y = d3.scale.linear()
+    .range([height, 0])
+    .domain([0, d3.max(data, function(d) { return d.value; })]);
 
-//Define X axis
+/*var x = d3.scale.ordinal()
+    .domain(function(d){return d.key})
+    .range(function(d){return d.value});*/
+
+var x = d3.scale.ordinal()
+    .domain(data.map(function(d) { return d.key; }))
+    .rangeRoundBands([0,width],.1);
+
+var barWidth = width / data.length;
+
 var xAxis = d3.svg.axis()
-		.scale(xScale)
-		.orient("bottom")//label position
-		.tickSize(5)
-    	.tickSubdivide(true);
+    .scale(x)
+    .orient("bottom");
 
-//Define Y axis
 var yAxis = d3.svg.axis()
-                  .scale(yScale)
-                  .orient("left")
-                  .ticks(5)
-                  .tickValues([1,3,5,7]);
+    .scale(y)
+    .orient("left")
+    .ticks(5);
 
-//Create SVG element
-var svg = d3.select("body")
-		.append("svg")
-		.attr("width", w)
-		.attr("height", h)
-		.attr("margin",margin);
+var chart = d3.select(".chart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .style("margin-left",15+"px");
 
-var brush = d3.svg.brush()
-    .x(xScale)
-    .on("brush", brushed);
+var bar = chart.selectAll("g")
+    .data(data)
+  .enter().append("g")
+    .attr("transform", function(d, i) { return "translate(" + x(d.key) + ",0)"; });
 
-function brushed() {
-  xScale.domain(brush.empty() ? xScale.domain() : brush.extent());//brush.extent() array with [0] lower extent : [1] upper extent
-  console.log(brush.extent());
-}
+var filterSpecies = [];
+chart.selectAll(".bar")
+    .data(data)
+  .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d) { return x(d.key); })
+    .attr("y", function(d) { return y(d.value); })
+    .attr("height", function(d) { return height - y(d.value); })
+    .attr("width", x.rangeBand())
+    .style("fill","steelblue")
+    .text(function(d) { return d.key;})
+    .on("click", function(){
+        var currentColor = d3.select(this)[0][0].style["fill"] == "steelblue" ? 'grey' : 'steelblue'
+        d3.select(this)[0][0].style["fill"] = currentColor;
+        var actVal = d3.select(this)[0][0].textContent;
+        filterSpecies.includes(actVal) == true ? filterSpecies.splice(filterSpecies.indexOf(actVal),1) : filterSpecies.push(actVal);
+        console.log(filterSpecies);
+        Species.filter(function(d){
+          return filterSpecies.indexOf(d) == -1;
+        });
+    });
 
-//Create bars
-svg.selectAll("rect")
-.data(SepalLength)
-.enter()
-.append("rect")
-.style("fill","teal")
-.style("margin-right", function(d) {
-    return barPadding + "px";
-})
-.style("opacity",function(d){
-	return 0.7
-})
-.attr("x", function(d, i) {
-		return xScale(i);
-})
-.attr("y", function(d) {
-		return yScale(d) - margin.bottom;
-})
-.attr("width", xScale.rangeBand())
-.attr("height", function(d) {
-		return (yScale(0) - yScale(d));
-})
-.on("click", function(){
-    var currentColor = d3.select(this)[0][0].style["fill"] == "teal" ? 'grey' : 'teal'
-    d3.select(this)[0][0].style["fill"] = currentColor;
-})
-.on("mouseover", function() {
-	var currentOpacity = d3.select(this)[0][0].style["opacity"] == 0.7 ? 1 : 0.7;
-    d3.select(this)[0][0].style["opacity"] = currentOpacity;
-  })
-.on("mouseout", function() {
-	var currentOpacity = d3.select(this)[0][0].style["opacity"] == 0.7 ? 1 : 0.7;
-    d3.select(this)[0][0].style["opacity"] = currentOpacity;
-  })
-.append("title")
-.text(function(d) {
-         return "This value is " + d;
-})
-;
-
-//Create X axis
-svg.append("g")
-	.attr("class", "axis") //Assign "axis" class
-	.attr("transform",'translate(0,' + (h - margin.bottom - margin.top) + ')')
-	.call(xAxis);
-//Create Y axis
-svg.append("g")
-	.attr("class", "axis")
-	.attr("transform", "translate(" + (padding + margin.left) + ","+ (-margin.bottom)+")")
-	.call(yAxis);
+chart.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
 
 
+chart.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+  .append("text") //Add chart title
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".75em")
+    .style("text-anchor", "end")
+    .text("Frequency");
 
-svg.append("g")
-  .attr("class", "x brush")
-  .call(brush)  //call the brush function, causing it to create the rectangles
-.selectAll("rect") //select all the just-created rectangles
-  .attr("y", -6)
-  .attr("height", h + 7); //set their height
